772f86209327559e01cf424cd0205ab094aff7d6
n.buzlov <n.buzlov@magnus-tech.ru>
2024-03-04 23:18:06 +0300

    feat(sandbox): new lib - @nx/node:lib --buildable=false --compiler=tsc

---
+++ sandbox/libs/lib-node-tsc-nb/src/lib/lib-node-tsc-nb.spec.ts
@@ -0,0 +1,7 @@
+import { libNodeTscNb } from './lib-node-tsc-nb';
+
+describe('libNodeTscNb', () => {
+  it('should work', () => {
+    expect(libNodeTscNb()).toEqual('lib-node-tsc-nb');
+  });
+});
