633a6fa6802c94cd75fe42f0d8716337952725d5
n.buzlov <n.buzlov@magnus-tech.ru>
2024-03-04 23:18:08 +0300

    feat(sandbox): new lib - @nx/node:lib --buildable=false --compiler=swc

---
+++ sandbox/libs/lib-node-swc-nb/src/lib/lib-node-swc-nb.spec.ts
@@ -0,0 +1,7 @@
+import { libNodeSwcNb } from './lib-node-swc-nb';
+
+describe('libNodeSwcNb', () => {
+  it('should work', () => {
+    expect(libNodeSwcNb()).toEqual('lib-node-swc-nb');
+  });
+});
