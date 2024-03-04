738ab7e1a65be3665d29c346e436167976a8fe9b
n.buzlov <n.buzlov@magnus-tech.ru>
2024-03-04 22:50:56 +0300

    feat(sandbox): new app - @nx/angular:app --bundler=esbuild --ssr=true

---
+++ sandbox/apps/app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'lego-root',
+  template: `<lego-nx-welcome></lego-nx-welcome>
+    <router-outlet></router-outlet>`,
+  styles: ``,
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class AppComponent {
+  title = 'app';
+}
